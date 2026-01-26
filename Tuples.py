d2290e18acb55dae8936f98cc2aec90315f914c2
Abhinesh Chandrabose <abhinesh191@gmail.com>
2026-01-26 11:48:40 +0000

    Tuples and Dictionary

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+# Example for Tuple in Python
+t1=(101,102,103)
+t2=("Apple","Banana","Cherry")
+t3=(101,"Apple",102,"Banana",103,"Cherry")
+print(t1)
+print(t2)
+print(t3)
+stuple = ("John",)
+print(stuple)
+print(t1[0],t1[1],t1[2])
+# t1[2]=202
+# TypeError: 'tuple' object does not support item assignment
+# print(t1)
