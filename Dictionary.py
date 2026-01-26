a3dc90108527a7825f10f8715e11b31bc367bae1
Abhinesh Chandrabose <abhinesh191@gmail.com>
2026-01-26 11:48:40 +0000

    Tuples and Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+# Example for Dictionary in Python
+student_info={ "Name":"Alice", "Age":20, "City":"London" }
+print(student_info)
+print(student_info["Name"])
+print(student_info["Age"])
+print(student_info["City"])
+# Changing the Name in the dictionary
+student_info["Name"]="Bob"
+print("Updated Name:",student_info["Name"])
+print("Complete student info:",student_info)
--- Listinpython.py
+++ Listinpython.py
@@ -9,3 +9,7 @@ print("First student:",students[0])
 print("Second student:",students[1])
 print("First mark:",marks[0])
 print("Second mark:",marks[1])
+# Adding a new student
+students[0]="Abhi"
+print("Updated first student:",students[0])
+print("Complete student list:",students)
