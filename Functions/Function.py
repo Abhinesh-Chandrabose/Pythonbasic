539326923ecebad80420def78ad1354ab434aa48
Abhinesh Chandrabose <abhinesh191@gmail.com>
2026-01-28 11:36:48 +0000

    Function Examples

---
+++ Functions/Function.py
@@ -0,0 +1,8 @@
+# Example for Function in Python
+def check_result(sname, mark):
+    if mark >= 40:
+        return f"{sname} has passed the exam."
+    else:
+        return f"{sname} has failed the exam."
+print(check_result("Alice", 89))
+
